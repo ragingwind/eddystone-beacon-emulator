739e1455b982074d2956e82f86df347753fa7813
ragingwind <ragingwind@gmail.com>
2015-07-30 23:54:21 +0900

    Fix intend

--- cli.js
+++ cli.js
@@ -11,10 +11,10 @@ var cli = meow({
 		'',
 		'Options',
 		'  --config Run into config service first and then start rest of advertisings services',
-		'	 --url URL for URL advertising',
+		'  --url URL for URL advertising',
 		'  --nid namespace ID, kind of FQDN or UUID, will be hashed and truncated in 10Byte',
-		'	 --bid Beacon ID for UID advertising, 6Byte',
-		'	 --tx TX Power start from -100 to 20, default is 0x12, will be interpreted as +18dBm',
+		'  --bid Beacon ID for UID advertising, 6Byte',
+		' --tx TX Power start from -100 to 20, default is 0x12, will be interpreted as +18dBm',
 		'  --volt Battery voltage, default is 0mV, or using a range like 500~10000 to randomize',
 		'  --temp Temperature, default is -128(0x8000), or using a range like -128~128 to randomize',
 		'  --tlm Frequency of TLM frame per 10 time of UID/URL advertising'
